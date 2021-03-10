e107bebf6d8a4b042d85484e2a94cd6e4616b96a
Irineo Ramos <irineo.ramos@codeimmersives.com>
2021-03-10 13:46:48 -0600

    1 pass.

--- main.js
+++ main.js
@@ -1,6 +1,54 @@
 // ┌─────────────────┐
 // │ Your code here! │	
 // └─────────────────┘
+function ageToAbilities(num) {
+    if ( num < 16 ){
+        return 'You can\'t drive.';   
+    } else if ( num >= 16 && num <= 17) {
+        return 'You can drive but not vote.';
+    } else if ( num >= 18 && num <= 24 ) {
+        return 'You can vote but not rent a car.';
+    } else {
+        return 'You can do pretty much anything.'
+    }
+}
+
+
+function oddIndices(array) {
+    let output = [];
+    for (const number of array) {
+        if (array % 2 !== 0)
+        output.push(array);
+    }
+    return output;
+    }
+
+
+function averageStringLength(str) {
+
+}
+
+
+function numOddValues(array) {
+    let odd = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 !== 0) {
+         odd.push(numbers[i]);
+    }
+}
+}
+
+
+
+function firstPunctuationIndex() {
+
+}
+
+
+
+function getPlace() {
+
+}
 
 
 
